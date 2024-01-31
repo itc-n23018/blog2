2d935f31b91a84703e77473630352739e5afd0e0
itc-n23018 <n23018@std.it-college.ac.jp>
2024-01-31 06:12:43 +0000

    Chapter2 終了

--- components/header.js
+++ components/header.js
@@ -1,5 +1,12 @@
+import Nav from 'components/nav'
+import Logo from 'components/logo'
 const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  )
 }
 
 export default Header
