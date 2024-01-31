8e73e7495c48f95bcbbbfab641f051a9e4df6bb4
itc-n23018 <n23018@std.it-college.ac.jp>
2024-01-31 08:07:09 +0000

    4.5まで終了

--- components/header.js
+++ components/header.js
@@ -1,10 +1,16 @@
+import Container from 'components/container'
 import Nav from 'components/nav'
 import Logo from 'components/logo'
+import styles from 'styles/header.module.css'
 const Header = () => {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <Container large>
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
+      </Container>
     </header>
   )
 }
