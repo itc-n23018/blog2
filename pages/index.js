3371815ae408c07e6920c15b532fa6a066aa8c9d
itc-n23018 <n23018@std.it-college.ac.jp>
2024-01-31 05:51:07 +0000

    2.9終了

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Hero from 'components/hero'
 const Home = () => {
   return (
     <>
-      <Hero title='CUBE' subtitle='アウトプットしていくサイト' />
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </>
   )
 }
