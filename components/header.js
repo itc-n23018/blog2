010e0394e61f38745e37c7f88eae4d5b2c24c77b
itc-n23018 <n23018@std.it-college.ac.jp>
2024-01-31 07:26:13 +0000

    作業するブランチをまちがえてるのでブランチをかえるためのコミット

--- components/header.js
+++ components/header.js
@@ -1,10 +1,13 @@
 import Nav from 'components/nav'
 import Logo from 'components/logo'
+import styles from 'styles/header.module.css'
 const Header = () => {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   )
 }
